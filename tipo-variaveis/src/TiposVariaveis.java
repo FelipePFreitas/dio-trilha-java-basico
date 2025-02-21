16f5b60ded847d30608d035482e8ac1ef34fd88a
Felipe Freitas <felipe.freitas1991@outlook.com>
2025-02-21 18:16:26 -0300

    TIPOS PRIMITIVOS

---
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hello, World!");
+
+        double salarioMinimo = 2500.33;
+        double salarioMinimo2 = 2.500; // errado
+
+        short numeroCurto =1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = numeroNormal; // fortemente tipado
+
+        final double VALOR_DE_PI =  3.14; //constante
+
+    }
+}
